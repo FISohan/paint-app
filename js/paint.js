67a978cd162894f7846aabfbb4d0dea21d0df7fa
FISohan <fisohan7@gmail.com>
2021-02-24 12:15:09 +0600

    costom line style  added

--- js/paint.js
+++ js/paint.js
@@ -1,22 +1,57 @@
 import { context, canvas } from "./main.js";
+
 export var config = {
-  isLIne: false,
+  isLIne: true,
   isShape: false,
   isErase: false,
 };
+var isDraw = false;
+var lineStyle = {
+  point: [],
+  lineWidth: 2,
+  color: document.querySelector("#input-color").value,
+  lineCap: "butt",
+  lineJoin: "round",
+  lineDash: [0, 0],
+};
+function update() {
+  lineStyle.lineWidth = document.getElementById("line-width").value;
+  lineStyle.color = document.getElementById("input-color").value;
+  lineStyle.lineCap = document.getElementById("line-cap").value;
+  lineStyle.lineJoin = document.getElementById("line-join").value;
+  lineStyle.lineDash = document.getElementById("line-dash").value.split(",");
+}
 
-function drawLine(end) {
+var line = [];
+
+function drawLine(end, lineStyle) {
   if (config.isLIne) {
+    context.lineWidth = lineStyle.lineWidth;
+    context.strokeStyle = `${lineStyle.color}`;
+    context.lineCap = `${lineStyle.lineCap}`;
+    context.lineJoin = `${lineStyle.lineJoin}`;
+    context.setLineDash([...lineStyle.lineDash]);
+    console.log();
     context.lineTo(...end);
     context.stroke();
+    console.log(lineStyle);
+    update();
   }
 }
 
 canvas.addEventListener("mousemove", (e) => {
-  if (e.altKey || e.ctrlKey) {
-    drawLine([e.clientX, e.clientY]);
-    // console.log("mouse move");
-  } else context.beginPath();
+  if (isDraw) {
+    drawLine([e.clientX, e.clientY], lineStyle);
+  }
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  isDraw = true;
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isDraw = false;
+  context.beginPath();
 });
 
 canvas.addEventListener("touchmove", (e) => {
@@ -27,3 +62,8 @@ canvas.addEventListener("touchmove", (e) => {
 canvas.addEventListener("touchend", (e) => {
   context.beginPath();
 });
+
+document.getElementById("x").addEventListener("click", show);
+function show() {
+  console.log(JSON.stringify(line));
+}
